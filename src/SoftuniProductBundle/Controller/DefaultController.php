c09324dbd2202b5ff589894edfb057af94c62103
ivaneurope <ivaneurope0@gmail.com>
2017-06-14 13:46:18 +0300

    Product bundle added

---
+++ src/SoftuniProductBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SoftuniProductBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('SoftuniProductBundle:Default:index.html.twig');
+    }
+}
---
+++ src/SoftuniProductBundle/SoftuniProductBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SoftuniProductBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SoftuniProductBundle extends Bundle
+{
+}
