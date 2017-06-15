acaf8a47b0bd1d2edeecae7d9dfd95d4dde3fad7
ivaneurope <ivaneurope0@gmail.com>
2017-06-15 18:23:16 +0300

    Initial ProductCategoryManager prototype

--- src/SoftuniProductBundle/Services/ProductCategoryManager.php
+++ src/SoftuniProductBundle/Services/ProductCategoryManager.php
@@ -14,5 +14,32 @@ use SoftuniProductBundle\Entity\ProductCategory;
 
 class ProductCategoryManager
 {
+    protected $em, $class, $container, $repository;
 
+    public function __construct(EntityManager $em, $class, $container)
+    {
+        $this->em = $em;
+        $this->class = $class;
+        $this->repository = $this->em->getRepository($class);
+        $this->container = $container;
+    }
+
+    public function createCategory()
+    {
+        $class = $this->getClass();
+        return new $class;
+    }
+
+    public function  removeCategory(ProductCategory $category, $andFlush = true)
+    {
+        $this->em->remove($category);
+        if ($andFlush) {
+            $this->em->flush();
+        }
+    }
+
+    public function getClass()
+    {
+        return $this->class;
+    }
 }
